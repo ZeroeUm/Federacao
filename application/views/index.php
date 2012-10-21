277d0a634c92a2d29df6428b4c424b9fb19bf13a
Humberto Menezes Bera <betao.tkd@gmail.com>
2012-10-21 16:10:51 -0200

    commit prj_ope 20121021 correção

--- application/views/index.php
+++ application/views/index.php
@@ -29,7 +29,7 @@
 
         <header id="header">
             <hgroup>
-                <h1 class="site_title"><a href="index.html">FEPAMI</a></h1>
+                <h1 class="site_title"><a href="#">FEPAMI</a></h1>
                 <h2 class="section_title">SISGAI</h2><div class="btn_view_site"><a href="#">Logout</a></div>
             </hgroup>
         </header> <!-- end of header bar -->
@@ -40,7 +40,7 @@
                 <!-- <a class="logout_user" href="#" title="Logout">Logout</a> -->
             </div>
             <div class="breadcrumbs_container">
-                <article class="breadcrumbs"><a href="index.html">Website Admin</a> <div class="breadcrumb_divider"></div> <a class="current">Dashboard</a></article>
+                <article class="breadcrumbs"><a href="#">Website Admin</a> <div class="breadcrumb_divider"></div> <a class="current">Dashboard</a></article>
             </div>
         </section><!-- end of secondary bar -->
 
@@ -152,21 +152,21 @@
                                     <td>11-0000-3333</td> 
                                     <td>mail@mail.com.br</td>
 
-                                    <td><input type="image" src="assets/images/icn_edit.png" title="Edit"><input type="image" src="assets/images/icn_trash.png" title="Trash"></td> 
+                                    <td><input type="image" src="<?php echo base_url() . 'assets/images/icn_edit.png'?>" title="Edit"><input type="image" src="<?php echo base_url() . 'assets/images/icn_trash.png'?>" title="Trash"></td> 
                                 </tr> 
                                 <tr> 
                                     <td><input type="checkbox"></td> 
                                     <td>Lorem Ipsum Dolor Sit Amet</td> 
                                     <td>11-2222-3333</td> 
                                     <td>mail@mail.com.br</td> 
-                                    <td><input type="image" src="assets/images/icn_edit.png" title="Edit"><input type="image" src="assets/images/icn_trash.png" title="Trash"></td> 
+                                    <td><input type="image" src="<?php echo base_url() . 'assets/images/icn_edit.png'?>" title="Edit"><input type="image" src="<?php echo base_url() . 'assets/images/icn_trash.png'?>" title="Trash"></td> 
                                 </tr> 
                                 <tr> 
                                     <td><input type="checkbox"></td> 
                                     <td>Lorem Ipsum Dolor Sit Amet</td> 
                                     <td>11-1111-3333</td> 
                                     <td>mail@mail.com.br</td> 
-                                    <td><input type="image" src="assets/images/icn_edit.png" title="Edit"><input type="image" src="assets/images/icn_trash.png" title="Trash"></td> 
+                                    <td><input type="image" src="<?php echo base_url() . 'assets/images/icn_edit.png'?>" title="Edit"><input type="image" src="<?php echo base_url() . 'assets/images/icn_trash.png'?>" title="Trash"></td> 
                                 </tr> 
 
 
@@ -175,14 +175,14 @@
                                     <td>Lorem Ipsum Dolor Sit Amet</td> 
                                     <td>11-2222-3333</td> 
                                     <td>mail@mail.com.br</td> 
-                                    <td><input type="image" src="assets/images/icn_edit.png" title="Edit"><input type="image" src="assets/images/icn_trash.png" title="Trash"></td> 
+                                    <td><input type="image" src="<?php echo base_url() . 'assets/images/icn_edit.png'?>" title="Edit"><input type="image" src="<?php echo base_url() . 'assets/images/icn_trash.png'?>" title="Trash"></td> 
                                 </tr> 
                                 <tr> 
                                     <td><input type="checkbox"></td> 
                                     <td>Lorem Ipsum Dolor Sit Amet</td> 
                                     <td>11-2222-3333</td> 
                                     <td>mail@mail.com.br</td> 
-                                    <td><input type="image" src="assets/images/icn_edit.png" title="Edit"><input type="image" src="assets/images/icn_trash.png" title="Trash"></td> 
+                                    <td><input type="image" src="<?php echo base_url() . 'assets/images/icn_edit.png'?>" title="Edit"><input type="image" src="<?php echo base_url() . 'assets/images/icn_trash.png'?>" title="Trash"></td> 
                                 </tr> 
                             </tbody> 
                         </table>
