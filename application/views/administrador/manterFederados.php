9d4ff700f3cdd56c41b300c695b644c5f547ce1a
Humberto Menezes Bera <betao.tkd@gmail.com>
2013-01-08 16:28:16 -0200

    commit prj_poe 20130108
    construção dos dropdown na view manterFederados funcionamento ainda não
    implementado
    remoção dos botões Profile e Message da layout
    falta fazer com que o funcionamento do change dos dropdowns da view
    manterFederados seja implementado totalmente

--- application/views/administrador/manterFederados.php
+++ application/views/administrador/manterFederados.php
@@ -1,10 +1,21 @@
-<?php
-
-?>
-<label>Instrutores</label>
+<script type="text/javascript">
+    var path = '<?php echo site_url(); ?>'
+</script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/manterFederados.js"></script>
+<div class="page_header">
 <?php 
+echo heading("Manutenção dos dados cadastrais dos federados", 4);
 $options = array("" => "Escolha uma opção.");
 foreach($instrutores as $instrutor)
     $options[$instrutor->id] = $instrutor->nome;
-echo form_dropdown("instrutores",$options);
+
+echo form_label("Instrutores","instrutores");
+echo form_dropdown("instrutores",$options,"",'id="instrutores"');
+echo form_label("Filiais","filiais");
+echo form_dropdown("filiais",array("" => "Escolha um instrutor primeiro"),"",'id="filiais"');
+echo form_label("Situação","situacao");
+echo form_dropdown("situacao",array("" => "Escolha uma filial primeiro"),"",'id="situacao"');
+echo form_label("Federados","federados");
+echo form_dropdown("federados",array("" => "Escolha uma situação primeiro"),"",'id="federados"');
 ?>
+</div>
--- application/views/header.php
+++ application/views/header.php
@@ -31,16 +31,6 @@
         </div>
         <div id="user-nav" class="navbar navbar-inverse">
             <ul class="nav btn-group">
-                <li class="btn btn-inverse"><a title="" href="#"><i class="icon icon-user"></i> <span class="text">Profile</span></a></li>
-                <li class="btn btn-inverse dropdown" id="menu-messages"><a href="#" data-toggle="dropdown" data-target="#menu-messages" class="dropdown-toggle"><i class="icon icon-envelope"></i> <span class="text">Messages</span> <span class="label label-important">5</span> <b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li><a class="sAdd" title="" href="#">new message</a></li>
-                        <li><a class="sInbox" title="" href="#">inbox</a></li>
-                        <li><a class="sOutbox" title="" href="#">outbox</a></li>
-                        <li><a class="sTrash" title="" href="#">trash</a></li>
-                    </ul>
-                </li>
-
                 <li class="btn btn-inverse"><a title="" href="login.html"><i class="icon icon-share-alt"></i> <span class="text">Logout</span></a></li>
             </ul>
         </div>
