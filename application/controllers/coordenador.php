00b64e6ad2d643b53fda8032fdc6f1efbcf61e49
felipe <felipe@notebook.(none)>
2013-02-23 15:52:19 -0300

    config

--- application/controllers/coordenador.php
+++ application/controllers/coordenador.php
@@ -14,95 +14,24 @@ class coordenador extends CI_Controller{
     
     
     
-    function professores(){
-        
-      $this->load->model('FilialModel');
-     
-      $this->data['dados'] = $this->FilialModel->get_filiais();
-       
-     
-      if($this->input->post()){
-      $this->data['filial'] = $this->input->post('filial');
-      $this->load->view('coordenador/modalidades',$this->data);
-      $this->load->view('footer');
-      }else{
-      $this->data['filial'] = "Nenhuma filial selecionada";
-      $this->load->view('coordenador/modalidades',$this->data);
-      $this->load->view('footer');
-      }
-      
-    }
     
-    function filiais(){
-        
-        $this->load->model('Modalidade_model','modalidade');
-        $this->load->model('filial_model','filiais');
-         $this->load->model('instrutores_model','instrutor');
-        
-         $data['instrutor'] = $this->instrutor->get_instrutores();
-         
-        $data['alunos'] = $this->modalidade->get_modalidade();
-       
-       
-        $this->load->view('header');
-        $this->load->view('coordenador/cadastroFiliais',$data);
-        $this->load->view('footer');
-        
-        
-         if($this->input->post()){
-             
-             $dados = $this->input->post();
-             
-          
-        
-             
-            if($this->filiais->set_filiais($dados)){
-                $this->session->set_flashdata('key', 'Salvo com sucesso');
-                redirect('/coordenador/index');
-            }else{
-                
-            };
-             
-             
-      
-        }
-    }
-    
-    function index(){
-        $this->load->view('header');
-        $this->load->view('coordenador/index');
-        $this->load->view('footer');
-    }
-    
-    function certificados(){
-        $this->load->view('header');
-        $this->load->view('devel');
-        $this->load->view('footer');
-    }
-    
-    function comprafaixas(){
-        $this->load->view('header');
-        $this->load->view('devel');
-        $this->load->view('footer');
-    }
-    
-    function prontuario(){
-        $this->load->view('header');
-        $this->load->view('devel');
-        $this->load->view('footer');
-    }
     
     function modalidade(){
+       $this->load->model('Modalidade_model', 'modalidades');
+        $data['categorias'] = $this->modalidades->get_modalidade();
         $this->load->view('header');
-        $this->load->view('devel');
+        $this->load->view('/coordenador/modalidade',$data);
         $this->load->view('footer');
     }
 
 
-    function graduados(){
-        $this->load->view('header');
-        $this->load->view('devel');
-        $this->load->view('footer');
+    function ajax_professores_modalidade($id_modalidade=null){
+        $this->load->model('Instrutor_model','instrutores');
+        
+        $this->instrutores->get_instrutor_modalidade('4');
+        die($id_modalidade);
+    
+        
     }
 }
 
