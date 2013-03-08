aa92ae8cd2c1266338819180b72a587df0c3cde4
felipe <felipe@notebook.(none)>
2013-03-08 00:10:54 -0300

    deletando projeto

--- application/views/devel.php
+++
@@ -1,6 +0,0 @@
-<div class="bs-docs-example" style="overflow: hidden;">
-            <blockquote class="pull-right">
-              <p>Pagina em desenvolvimento</p>
-              <small>Falta a criação da view</small>
-            </blockquote>
-          </div>
