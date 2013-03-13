eacd7696a7062a31887c9da57ef81c9b2c1a10a2
Felipe <felipe@chipsetdesenvolvimento.com>
2013-03-13 10:30:40 -0300

    removendo arquivos do master

--- application/views/coordenador/modalidade.php
+++
@@ -1,6 +0,0 @@
-Informe uma modalidade: 
-<select>
- <?php foreach ($categorias as $r=>$d){?>
-    <option value="<?php echo $d['id_modalidade'] ?>"><?php echo $d['nome'] ;?></option>
-<?php } ?> 
-</select>
