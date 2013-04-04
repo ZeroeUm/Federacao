e08f0a5abbd77a69e971799b75f07c332fbc9263
Felipe <felipe@felipe-RF511-RF411-RF711.(none)>
2013-04-03 22:13:56 -0300

    atualização

---
+++ application/views/login.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * 2013-03-05
+ * @author Humberto
+ */
+$attr = array(
+    'class' => 'form-horizontal',
+    'id' => 'frmLogin',
+    'name' => 'frmLogin'
+);
+$label = array(
+    'class' => 'control-label'
+);
+?>
+<html>
+    <head>
+        <title>Login ao sistema da Federação</title>
+        <meta http-equiv="content-type" content="text/html" charset="ISO-8859-1" />
+        <script src="<?php echo base_url(); ?>assets/js/jquery-1.8.2.js"></script>
+        <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap-responsive.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.css">
+
+    </head>
+    <body>
+        <div class="row-fluid">
+            <div class="alert-error">
+                <?php echo validation_errors(); ?>
+            </div>
+            <?php
+            echo form_fieldset('Área de login ao SGAI');
+            echo form_open('login', $attr);
+            ?>
+            <div class="control-group">
+                <?php echo form_label('Usuário', 'usuario', $label); ?>
+                <div class="controls">
+                    <?php echo form_input('usuario', '', 'id="usuario" class="span3" maxlength="20" required placeholder="Usuário"'); ?>
+                </div>
+            </div>
+            <div class="control-group">
+                <?php echo form_label('Senha', 'senha', $label); ?>
+                <div class="controls">
+                    <?php echo form_password('senha', '', 'id="senha" class="span3" maxlength="10" required placeholder="Senha"'); ?>
+                </div>
+            </div>
+            <div class="controls">
+                <?php
+                echo form_submit('btnEntrar', 'Entrar', 'id="btnEntrar" class="btn btn-primary"');
+                ?>    
+            </div>
+            <?php
+            echo form_close();
+            echo form_fieldset_close();
+            ?>
+        </div>
+    </body>
+</html>
---
+++ application/views/testando.php
@@ -0,0 +1,15 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ola Mundo</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+    </head>
+    <body>
+        <h1>Testando</h1>
+        <h3>Exmeplo de view</h3>
+    </body>
+</html>
