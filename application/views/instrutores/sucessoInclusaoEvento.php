54c994036b10a79d02b591f999d73184877fdf49
Marcos Motta <mrcsmotta1@ig.com.br>
2013-04-24 19:41:40 -0300

    View de resposta após incluir federado no evento.

---
+++ application/views/instrutores/sucessoInclusaoEvento.php
@@ -0,0 +1,12 @@
+
+
+<div class="<?php echo(($status)?  'alert-success':'alert-error'); ?>">
+    <b>
+    <?php 
+        echo $msg;
+        echo anchor('instrutores/Inscricao','Clique aqui para voltar á area de Inscrição de Federados em Evento');
+    ?>
+    </b>
+</div>
+
+
