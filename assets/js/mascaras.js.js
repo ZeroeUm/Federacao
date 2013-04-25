a647248249ad9c63f8e0aef73315f4293d9a524f
Felipe <felipe@chipsetdesenvolvimento.com>
2013-04-25 15:33:07 -0300

    atualização

---
+++ assets/js/mascaras.js.js
@@ -0,0 +1,50 @@
+$('document').ready(function(){
+ 
+    $('#rg').setMask('99.999.999-99');
+    $('#cnpj').setMask('999.999.999/9999-99');
+    $('#cpf').setMask('999.999.999-99');
+    $('#cep').setMask('99999-999');
+    $('#telefone').setMask("(99)9999-9999"); 
+    
+    $('#celular').setMask("(99)9999-99999").ready(function(event) {
+        var target, phone, element;
+        target = (event.currentTarget) ? event.currentTarget : event.srcElement;
+        phone = target.value.replace(/\D/g, '');
+        element = $(target);
+        element.unsetMask();
+        if(phone.length > 10) {
+            element.setMask("(99)99999-9999");
+        } else {
+            element.setMask("(99)9999-99999");
+        }
+    });
+    
+    
+    
+    $("#email").focusout(function(){
+        //at$(".msg").hide();ribuindo o valor do campo
+        var sEmail	= $("#email").val();
+        // filtros
+        var emailFilter=/^.+@.+\..{2,}$/;
+        var illegalChars= /[\(\)\<\>\,\;\:\\\/\"\[\]]/
+        // condição
+        if(!(emailFilter.test(sEmail))||sEmail.match(illegalChars)){
+            if($(this).val()==''){
+                $(this).css({
+                    "border-color" : "#F00", 
+                    "padding": "2px"
+                });
+            }else{
+                alert('formato do email é invalido');
+            }
+        }else{
+            $(this).css({
+                    "border-color" : "", 
+                    "padding": "2px"
+                });
+        }
+    });
+    
+    
+})
+
