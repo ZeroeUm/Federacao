99dff853242bf81c7f9cc57de6a3f394c8a5e13a
Felipe <felipe@chipsetdesenvolvimento.com>
2013-05-20 17:31:13 -0300

    tela de login

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -5,48 +5,56 @@
  * @author Humberto
  */
 
-class Login extends CI_Controller
-{
+class Login extends CI_Controller {
 
-    function __construct()
-    {
+    function __construct() {
         parent::__construct();
         $this->load->model("Login_model", 'login', TRUE);
     }
 
-    function lembrar_senha(){
-        
-        
-        if($this->input->post()){
-          $email = $this->input->post('email');
-         //Localizar email
-         $dados = $this->login->get_email($email); 
-        
-         //Mandar a senha por email
-         $this->load->library('email'); 
-         $this->email->from('elder.f.silva@gmail.com', 'Felipe');
-         $this->email->to('felipe@chipsetdesenvolvimento.com');
-         $this->email->subject('Assunto');
-         
-         $mensagem = "Olá,<strong> {$dados['0']['nome']}</strong><br>
+    function lembrar_senha() {
+
+
+        if ($this->input->post()) {
+
+            $email = $this->input->post('email');
+            
+            if($email==''){
+               $this->session->set_flashdata('alerta', 'Nenhum email foi informado');
+                redirect('/login/lembrar_senha'); 
+                
+            }
+            //Localizar email
+            $dados = $this->login->get_email($email);
+
+            //Mandar a senha por email
+            $this->load->library('email');
+            $this->email->from('elder.f.silva@gmail.com', 'Felipe');
+            $this->email->to('felipe@chipsetdesenvolvimento.com');
+            $this->email->subject('Assunto');
+
+            $mensagem = "Olá,<strong> {$dados['0']['nome']}</strong><br>
                       Sua solicitação de senha foi realizada.
                       <br>
                       <br>
                       <strong>Usuário:</strong> {$dados['0']['nome']}<br>
                       <strong>Senha:</strong> {$dados['0']['senha']}";
-         $this->email->message($mensagem);
-         $this->email->send();
-         
+            $this->email->message($mensagem);
+            if ($this->email->send()) {
+                $this->session->set_flashdata('alerta', 'um email enviado com sua senha');
+                redirect('/login/lembrar_senha');
+            } else {
+                $this->session->set_flashdata('alerta', 'Não foi possível reenviar sua senha, contate seu instrutor.');
+                redirect('/login/lembrar_senha');
+            };
+        }else{
+            $this->load->view('lembrar_senha');
         }
-        
-        $this->load->view('lembrar_senha');
-        
-        
+
         
     }
-            
-    function index()
-    {
+
+    function index() {
         $this->form_validation->set_rules('usuario', 'Usuário', 'trim|xss_clean|required');
         $this->form_validation->set_rules('senha', 'Senha', 'trim|callback_verificar_banco|xss_clean|required');
 
@@ -57,8 +65,7 @@ class Login extends CI_Controller
         endif;
     }
 
-    function verificar_banco($senha)
-    {
+    function verificar_banco($senha) {
         if ($this->verificaStatus($this->input->post('usuario'), $senha)):
             return TRUE;
         else:
@@ -67,13 +74,12 @@ class Login extends CI_Controller
         endif;
     }
 
-    function verificaStatus($usuario, $senha)
-    {
+    function verificaStatus($usuario, $senha) {
         if ($this->login->verificarStatus($usuario)):
             if ($this->login->login($usuario, $senha)):
                 $resultado = $this->login->IDFedereado($usuario, $senha);
                 $dadosUsuario = $this->login->dadosUsuario($resultado[0]['id_federado']);
-                
+
                 $this->session->set_userdata($dadosUsuario[0]);
                 $this->session->set_userdata('autentificado', TRUE);
                 return TRUE;
@@ -87,35 +93,33 @@ class Login extends CI_Controller
         endif;
     }
 
-    function logoff()
-    {
+    function logoff() {
         if ($this->session->userdata('autentificado'))
             $this->session->sess_destroy();
         redirect('login', 'refresh');
     }
-    
-    function trocarSenha($usuario)
-    {
-        $this->form_validation->set_rules('novaSenha','Nova senha','trim|max_length[10]|xss_clean|required');
-        $this->form_validation->set_rules('confirmar','Confirmação de senha','trim|max_length[10]|matches[novaSenha]|xss_clean|required');
-        if($this->form_validation->run() == FALSE):
+
+    function trocarSenha($usuario) {
+        $this->form_validation->set_rules('novaSenha', 'Nova senha', 'trim|max_length[10]|xss_clean|required');
+        $this->form_validation->set_rules('confirmar', 'Confirmação de senha', 'trim|max_length[10]|matches[novaSenha]|xss_clean|required');
+        if ($this->form_validation->run() == FALSE):
             $this->load->view('header');
             $dados['usuario'] = $usuario;
-            $this->load->view('trocarSenha',$dados);
+            $this->load->view('trocarSenha', $dados);
             $this->load->view('footer');
         else:
             $this->trocar();
         endif;
     }
-    
-    function trocar()
-    {
+
+    function trocar() {
         $senha = $this->input->post('novaSenha');
         $usuario = $this->input->post('federado');
         $dados = array('senha' => $senha);
-        $this->login->trocarSenha($usuario,$dados);
+        $this->login->trocarSenha($usuario, $dados);
         $this->logoff();
     }
+
 }
 
 ?>
