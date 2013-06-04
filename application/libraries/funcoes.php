02e4d89ca63debd775aa074dc71ff82750ff623a
Felipe <felipe@chipsetdesenvolvimento.com>
2013-06-04 19:11:59 -0300

    versão de produção

--- application/libraries/funcoes.php
+++ application/libraries/funcoes.php
@@ -1,9 +1,6 @@
 
 <?php
-
-if (!defined('BASEPATH'))
-    exit('No direct script access allowed');
-
+if (!defined('BASEPATH'))exit('No direct script access allowed');
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -17,9 +14,7 @@ if (!defined('BASEPATH'))
 class Funcoes {
     
     function calcular_idade($data){
-        
-    }
-
+      }
     function idade($data_completa){
         $data = explode('-', $data_completa);
         
@@ -29,43 +24,29 @@ class Funcoes {
             $idade = date('Y') - $data['2']; 
             $idade = $idade-1;
         }
-        
-        
         return $idade;
     }
-
-
     function tratar_timestamp($data, $tipo=0) {
 //       tipo1 = mostra o horarário
 //       tipo0 = não mostra o horário
-
         if ($tipo == 0) {
-        
             $datetime = explode(' ',$data);
             $pre_data = explode('-',$datetime['0']);
             return $pre_data['2']."-".$pre_data['1']."-".$pre_data['0'];
-            
         } else {
-            
             $datetime = explode(' ', $data);
             $pre_data = explode('-', $datetime['0']);
             return $pre_data['2']."-".$pre_data['1']."-".$pre_data['0']."  ".$datetime['1'];
-            
         }
     }
-
     function imprimir($dados) {
-
         echo "<pre>";
         print_r($dados);
         echo "</pre>";
     }
-
     function data($data) {
         $data = explode("-", $data);
         return $data[2] . "-" . $data[1] . "-" . $data[0];
     }
-
 }
-
 ?>
